78d2a6dc72061220dab03e58c1354535eae82e7b
tomonari <zcsxcacg@i.softbank.jp>
2022-11-17 22:29:40 +0900

    Vueでカレンダー表示用ページを作成した

---
+++ app/javascript/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>{{ state.message }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    return {
+      state: {
+        message: "Hello Vue3 "
+      }
+    }
+  }
+})
+</script>
