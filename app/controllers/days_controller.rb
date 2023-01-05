ab4d702e12fde80069462a641dba2aa38aef9e92
tomonari <zcsxcacg@i.softbank.jp>
2023-01-05 20:13:18 +0900

    days_controllerの処理をbefore_actionで共通化した

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,17 +1,26 @@
 # frozen_string_literal: true
 
 class DaysController < ApplicationController
+  before_action :set_calendar
+  before_action :set_target_day
+
   def destroy
-    @calendar = User.find(current_user.id).calendars.find_by(year: params[:year])
-    target_day = Date.new(params[:year].to_i, params[:month].to_i, params[:date].to_i)
-    @day = @calendar.days.find_by(date: target_day)
-    @day.delete if @day
+    day = @calendar.days.find_by(date: @target_day)
+    day.delete if day
   end
 
   def update
+    day = @calendar.days.find_or_create_by(date: @target_day)
+    Day.update!(day.id, schedule: params[:schedule])
+  end
+
+  private
+
+  def set_calendar
     @calendar = User.find(current_user.id).calendars.find_by(year: params[:year])
-    target_day = Date.new(params[:year].to_i, params[:month].to_i, params[:date].to_i)
-    @day = @calendar.days.find_or_create_by(date: target_day)
-    Day.update!(@day.id, schedule: params[:schedule])
+  end
+
+  def set_target_day
+    @target_day = Date.new(params[:year].to_i, params[:month].to_i, params[:date].to_i)
   end
 end
